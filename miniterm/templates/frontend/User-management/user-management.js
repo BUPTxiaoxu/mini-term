2b86cc1c9bb660577f5775a68fbb8e1426cd1822
Harryan-xu <272112880@qq.com>
2024-10-03 08:38:04 +0800

    front-end

--- miniterm/templates/frontend/User-management/user-management.js
+++ miniterm/templates/frontend/User-management/user-management.js
@@ -29,4 +29,98 @@ function deleteRow(btn) {
     var row = btn.parentNode.parentNode;
     var table = document.getElementById("user-list");
     table.deleteRow(row.rowIndex);
+
+    //delete from database here 
+    fetch('/user/delete', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'X-CSRFToken': '{{ csrf_token }}',  // Django CSRF 令牌
+        },
+        body: new URLSearchParams({
+            name: row.cells[0].innerHTML,
+            password: row.cells[1].innerHTML,
+            role: row.cells[2].innerHTML,
+            access_level: row.cells[3].innerHTML,
+            authorization: row.cells[4].innerHTML,
+        })//这里接口文档参数是UserID
+    })
+   .then(response => response.json())
+   .then(data => {
+        console.log(data);
+        if (data.status === '200') {
+          alert("Deleted successfully");
+        } else {
+          alert("Failed to delete data");
+        }
+    })
+   .catch(error => {
+    console.error(error);
+    alert("Failed to delete data");
+    });
 }
+
+//編輯
+function editRow(btn) {
+    var row = btn.parentNode.parentNode;
+    var nameCell = row.cells[0];
+    var passwordCell = row.cells[1];
+    var roleCell = row.cells[2];
+    var accessLevelCell = row.cells[3];
+    var authorizationCell = row.cells[4];
+    var actionCell = row.cells[5];
+    nameCell.innerHTML = "<input type='text' value='" + nameCell.innerHTML + "'>";
+    passwordCell.innerHTML = "<input type='text' value='" + passwordCell.innerHTML + "'>";
+    roleCell.innerHTML = "<input type='text' value='" + roleCell.innerHTML + "'>";
+    accessLevelCell.innerHTML = "<input type='text' value='" + accessLevelCell.innerHTML + "'>";
+    authorizationCell.innerHTML = "<input type='text' value='" + authorizationCell.innerHTML + "'>";
+    actionCell.innerHTML = "<button onclick='saveRow(this)'>Save</button><button onclick='cancelEdit(this)'>Cancel</button>";
+}
+
+function saveRow(btn) {
+    var row = btn.parentNode.parentNode;
+    var nameCell = row.cells[0];
+    var passwordCell = row.cells[1];
+    var roleCell = row.cells[2];
+    var accessLevelCell = row.cells[3];
+    var authorizationCell = row.cells[4];
+    var actionCell = row.cells[5];
+    nameCell.innerHTML = nameCell.getElementsByTagName("input")[0].value;
+    passwordCell.innerHTML = passwordCell.getElementsByTagName("input")[0].value;
+    roleCell.innerHTML = roleCell.getElementsByTagName("input")[0].value;
+    accessLevelCell.innerHTML = accessLevelCell.getElementsByTagName("input")[0].value;
+    authorizationCell.innerHTML = authorizationCell.getElementsByTagName("input")[0].value;
+    actionCell.innerHTML = "<button onclick='editRow(this)'>Edit</button><button onclick='deleteRow(this)'>Remove</button>";
+
+    //save to database here 
+    fetch('/user/add', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'X-CSRFToken': '{{ csrf_token }}',  // Django CSRF 令牌
+        },
+        body: new URLSearchParams({
+            name: nameCell.getElementsByTagName("input")[0].value,
+            password: passwordCell.getElementsByTagName("input")[0].value,
+            role: roleCell.getElementsByTagName("input")[0].value,
+            access_level: accessLevelCell.getElementsByTagName("input")[0].value,
+            authorization: authorizationCell.getElementsByTagName("input")[0].value,
+        })
+    })
+   .then(response => response.json())
+   .then(data => {
+        console.log(data);
+        if (data.status === '200') {
+          alert("Saved successfully");
+        } else {
+          alert("Failed to save data");
+        }
+    })
+   .catch(error => {
+    console.error(error);
+    alert("Failed to save data");
+    });
+}
+
+
+
